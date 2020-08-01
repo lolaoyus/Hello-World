0789e27de3425b608927c77ba72c6570b88e4386
lolaoyus <65992615+lolaoyus@users.noreply.github.com>
2020-07-31 23:28:08 -0600

    Data 475 Lab 1

--- Hello_world.py
+++ Hello_world.py
@@ -1,12 +1,12 @@
-name = input("What is your name? ")
-birth_year=input("when were you born ?")
+name=input("what is your name?")
+birth_year=(input("when were you born?"))
 
-print(f"hello, {name}!")
+print(f"Hello, {name}")
 
+from datetime import datetime
+this_year = datetime.now().year
 
-# from datetime import datetime
-# this_year = datetime.now().year
+age = this_year-int(birth_year)
 
-
-# print(f"You must be {age} ")
-# print("goodbye")
+print(f"you must be {age}")
+print(birth_year)
